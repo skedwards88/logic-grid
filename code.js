4787dafec3e55bc60ea9c1d30e0900b3015cd6ed
skedwards88 <skedwards88@github.com>
2021-11-30 08:36:24 -0800

    update to multid array

--- code.js
+++ code.js
@@ -1,14 +1,15 @@
-const keys = [
-  ["a", "b", "c", "d"],
+const groups = [
+  // ["a", "b", "c", "d"],
   [1, 2, 3, 4],
-  [10, 20, 30, 40]
+  [10, 20, 30, 40],
+  [100, 200, 300, 400]
 ]
 
 const answers = [
-  ['a', 1, 10],
-  ['b', 2, 20],
-  ['c', 3, 30],
-  ['d', 4, 40]
+  [1, 10, 100],
+  [2, 20, 200],
+  [3, 30, 300],
+  [4, 40, 400]
 ] // todo in future, randomly generate this
 
 const operators = {
@@ -32,33 +33,15 @@ function pickRandom(inputArray) {
   return inputArray[Math.floor(Math.random() * inputArray.length)];
 }
 
-function makeGridOfSize(size) {
-  return Array.from({ length: size }, () => (Array.from({ length: size }, () => null)
-  ))
+function makeArray(size, value = null) {
+  return Array.from({ length: size }, () => value)
 }
 
-function makeLogicGrids(keys) {
-  const size = keys[0].length
-
-  let logicGrids = {}
-  keys.slice(0, keys.length - 1).map((_, index) => (
-    logicGrids[index] = {}
-  ))
-  keys.map((_, index) => (
-    keys.slice(index + 1).map((_, index2) => (
-      logicGrids[index][index2 + index + 1] = {
-        grid: makeGridOfSize(size),
-        rowParentIndex: index,
-        colParentIndex: (index2 + index + 1),
-        rowHeader: keys[index],
-        colHeader: keys[index2 + index + 1]
-      }
-    ))
-  ))
-
-  return logicGrids
+function makeLogicArray(groups) {
+  let assembledGrid = null
+  groups.forEach(group => (assembledGrid = makeArray(group.length, JSON.parse(JSON.stringify(assembledGrid)))))
+  return assembledGrid
 }
-
 
 function findValidClue() {
   let foundOperator = false
@@ -69,11 +52,11 @@ function findValidClue() {
     const answer = pickRandom(answers)
 
     // randomly select an item in the answer (todo later can make work with non-numbers)
-    const item = pickRandom(answer)
-    const index = answer.indexOf(item)
+    const item = pickRandom(answer) // e.g. 10
+    const index = answer.indexOf(item) // corresponds to the group index of the item (e.g. 10 => 2)
 
-    // randomly select a possible answer from the corresponding type
-    const rightComparison = pickRandom(keys[index])
+    // randomly select a possible answer from the corresponding group
+    const rightComparison = pickRandom(groups[index]) // e.g. 10, 20, 30, 40
 
     // randomly select an operator
     const operator = pickRandom(Object.keys(operators))
@@ -94,12 +77,18 @@ function findValidClue() {
 
       // console.log(`FOUND VALID CLUE: ${leftComparison} (${item}) ${operator} ${rightComparison}`)
       clue = {
-        left: leftComparison,
-        leftIndex: leftIndex,
-        original: item,
-        operator: operator,
-        right: rightComparison,
-        rightIndex: index
+        leftGroupIndex: leftIndex, // index of the group
+        leftItemIndex: groups[leftIndex].indexOf(leftComparison), // index of the item in the group
+        rightGroupIndex: index,
+        rightItemIndex: groups[index].indexOf(rightComparison),
+        operator: operator, // e.g. <
+
+        // left: leftComparison, // e.g. a
+        // leftIndex: leftIndex, // first group index, e.g. 0
+        // original: item, // e.g. 10
+        // operator: operator, // e.g. <
+        // right: rightComparison, // e.g. 30
+        // rightIndex: index // second group index, e.g. 2
       }
     }
   }
@@ -107,11 +96,18 @@ function findValidClue() {
   return clue
 }
 
+function flatten(array) {
+  while (Array.isArray(array[0])) {
+    array = flatten(array.flat())
+  }
+  return array
+}
+
 function generateClue() {
   let count = 0
 
   // make logic grid
-  let deduced = makeLogicGrids(keys)
+  let deduced = makeLogicArray(groups)
 
   let foundAllClues = false
   let foundClue = false
@@ -124,53 +120,32 @@ function generateClue() {
 
       const clue = findValidClue()
 
-      // find the grid that corresponds to the clue
       let deducedCopy = JSON.parse(JSON.stringify(deduced))
-      const category1 = Math.min(clue.leftIndex, clue.rightIndex)
-      const category2 = Math.max(clue.leftIndex, clue.rightIndex)
 
-      // find the row/col in the grid that correspond to the clue
-      const rowNum = clue.leftIndex < clue.rightIndex ? keys[clue.leftIndex].indexOf(clue.left) : keys[clue.rightIndex].indexOf(clue.right)
-      if (rowNum < 0) { console.log("ERROR 1!!!!") }
-      const colNum = clue.leftIndex < clue.rightIndex ? keys[clue.rightIndex].indexOf(clue.right) : keys[clue.leftIndex].indexOf(clue.left)
-      if (colNum < 0) { console.log("ERROR 2!!!!") }
-
-      // For each item in the row or col, mark the false values according to the clue
-      if (clue.leftIndex < clue.rightIndex) {
-        for (let index = 0; index < deducedCopy[category1][category2].grid.length; index++) {
-          if (deducedCopy[category1][category2].grid[rowNum][index] === null && !operators[clue.operator](keys[clue.rightIndex][index], clue.right)) {
-            deducedCopy = setToFalse({ category1: category1, category2: category2, rowIndex: rowNum, colIndex: index, deduced: deducedCopy })
-          }
-        }
-      } else {
-        for (let index = 0; index < deducedCopy[category1][category2].grid.length; index++) {
-          if (deducedCopy[category1][category2].grid[index][colNum] === null && !operators[clue.operator](keys[clue.rightIndex][index], clue.right)) {
-            deducedCopy = setToFalse({ category1: category1, category2: category2, rowIndex: index, colIndex: colNum, deduced: deducedCopy })
-          }
+      // hold the leftComparison constant
+      // iterate across the rightComparison index, and set to false depending on operator result (operator(currentRightIteration, rightComparison) )
+      // 
+      for (let itemIndex = 0; itemIndex < groups[clue.rightGroupIndex].length; itemIndex++) {
+        if (!operators[clue.operator](groups[clue.rightGroupIndex][itemIndex], groups[clue.rightGroupIndex][clue.rightItemIndex])) {
+          deducedCopy = setToFalse({[clue.leftGroupIndex]: clue.leftItemIndex, [clue.rightGroupIndex]: itemIndex}, deducedCopy)
         }
       }
 
       // if the clue narrowed down the possible answers,
       // update the possible answers and stop looking
-      if (deduced[category1][category2].grid.toString() !== deducedCopy[category1][category2].grid.toString()) {
+      if (deduced.toString() !== deducedCopy.toString()) {
         deduced = deducedCopy
         foundClue = true
-        console.log(`CLUE: ${clue.left} (${clue.original}) ${clue.operator} ${clue.right}`)
-        console.log('DEDUCED:')
-        console.log(keys.flatMap((_, index) => (
-          keys.slice(index + 1).map((_, index2) => (
-            deduced[index][index2 + index + 1].grid
-          ))
-        )))
-        console.log('======')  
+        console.log(`CLUE ${count}: ${clue.leftGroupIndex}, ${clue.leftItemIndex} (${groups[clue.leftGroupIndex][clue.leftItemIndex]}) ${clue.operator} ${clue.rightGroupIndex}, ${clue.rightItemIndex} (${groups[clue.rightGroupIndex][clue.rightItemIndex]}) results:`)
+        console.log(JSON.stringify(deduced))
         clues.push(clue)
       } else {
         // console.log(`TOSSED: ${clue.left} (${clue.original}) ${clue.operator} ${clue.right}`)
       }
 
 
-      if (keys.flatMap((_, index) => (
-        keys.slice(index + 1).flatMap((_, index2) => (
+      if (groups.flatMap((_, index) => (
+        groups.slice(index + 1).flatMap((_, index2) => (
           deduced[index][index2 + index + 1].grid
         ))
       )).flat().every(i => i != null)) {
@@ -184,7 +159,7 @@ function generateClue() {
         count += 1
       }
 
-      if (count > 100) {
+      if (count > 50) {
         console.log('NOT FOUND AFTER 100 rounds')
         foundAllClues = true
         foundOperator = true
@@ -208,74 +183,49 @@ function generateClue() {
 
 
 
-function setToTrue({ category1, category2, rowIndex, colIndex, deduced }) {
+function setToFalse(indexesToSet, grid) {
+  for (let groupIndex = 0; groupIndex < groups.length; groupIndex++) {
+    // if we don't have a value for this group index, call this recursive function for each value in the group
+    if (indexesToSet[groupIndex] === undefined) {
+      for (let groupItemIndex = 0; groupItemIndex < groups[groupIndex].length; groupItemIndex++) {
+        const newIndexes = {
+          ...indexesToSet,
+          [groupIndex]: groupItemIndex
+        }
+        grid = setToFalse(newIndexes, grid)
+      }
+    }
 
-  let deducedCopy = JSON.parse(JSON.stringify(deduced))
-
-  // sets the index to true, and sets all others in the row/col to false
-  // console.log(`setting ${rowIndex} ${colIndex} to TRUE:  ${JSON.stringify(grid)}`)
-
-  // if the value is already true, return early
-  if (deducedCopy[category1][category2].grid[rowIndex][colIndex] === true) {
-    return deduced
-  }
-
-  // set the index to true
-  deducedCopy[category1][category2].grid[rowIndex][colIndex] = true
-
-  // set the whole row to false
-  for (let index = 0; index < deducedCopy[category1][category2].grid.length; index++) {
-    if (index !== rowIndex) {
-      deducedCopy = setToFalse({ category1: category1, category2: category2, rowIndex: index, colIndex: colIndex, deduced: deducedCopy })
+    // if we have an index value for every group, set the index to false and return the new array
+    if (groups.every((_, index) => indexesToSet[index] !== undefined)) {
+      // console.log(`SETTING ${JSON.stringify(indexesToSet)}`)
+      // hardcoding isn't ideal, but I didn't figure out a clean way to do this
+      switch (groups.length) {
+        case 1:
+          grid[indexesToSet[0]] = false
+          break;
+        case 2:
+          grid[indexesToSet[0]][indexesToSet[1]] = false
+          break;
+        case 3:
+          grid[indexesToSet[0]][indexesToSet[1]][indexesToSet[2]] = false
+          break;
+        case 4:
+          grid[indexesToSet[0]][indexesToSet[1]][indexesToSet[2]][indexesToSet[3]] = false
+          break;
+        case 5:
+          grid[indexesToSet[0]][indexesToSet[1]][indexesToSet[2]][indexesToSet[3]][indexesToSet[4]] = false
+          break;
+        case 6:
+          grid[indexesToSet[0]][indexesToSet[1]][indexesToSet[2]][indexesToSet[3]][indexesToSet[4]][indexesToSet[5]] = false
+          break;
+        default:
+          throw new Error(`Array of ${groups.length} not supported`)
+      }
     }
   }
 
-  // set the whole column to false
-  for (let index = 0; index < deducedCopy[category1][category2].grid.length; index++) {
-    if (index !== colIndex) {
-      deducedCopy = setToFalse({ category1: category1, category2: category2, rowIndex: rowIndex, colIndex: index, deduced: deducedCopy })
-    }
-  }
-
-  return deducedCopy
-}
-
-function setToFalse({ category1, category2, rowIndex, colIndex, deduced }) {
-
-  let deducedCopy = JSON.parse(JSON.stringify(deduced))
-  
-  // sets the index to false, and if all but one in the
-  // row or col is false, sets the remaining index to true
-
-  // if the value is already false, return early
-  if (deducedCopy[category1][category2].grid[rowIndex][colIndex] === false) {
-    return deduced
-  }
-
-  // set the index to false
-  deducedCopy[category1][category2].grid[rowIndex][colIndex] = false
-
-  // Check if that resulted in the row having only a single null remaining
-  const totalFalseInRow = deducedCopy[category1][category2].grid[rowIndex].reduce((tallyFalse, currentValue) => currentValue === false ? tallyFalse += 1 : tallyFalse += 0, 0)
-  // if yes, set that remaining index to true (unless it is already true)
-  if (totalFalseInRow === deducedCopy[category1][category2].grid.length - 1) {
-    const trueIndex = deducedCopy[category1][category2].grid[rowIndex].findIndex(i => i === null)
-    if (trueIndex >= 0) {
-      deducedCopy = setToTrue({ category1: category1, category2: category2, rowIndex: rowIndex, colIndex: trueIndex, deduced: deducedCopy })
-    }
-  }
-
-  // Check if that resulted in the column being all false but one
-  const totalFalseInCol = deducedCopy[category1][category2].grid.reduce((tallyFalse, currentRow) => currentRow[colIndex] === false ? tallyFalse += 1 : tallyFalse += 0, 0)
-  // if yes, set that remaining index to true
-  if (totalFalseInCol === deducedCopy[category1][category2].grid.length - 1) {
-    const trueIndex = deducedCopy[category1][category2].grid.map(row => row[colIndex]).findIndex(i => i === null)
-    if (trueIndex >= 0) {
-      deducedCopy = setToTrue({ category1: category1, category2: category2, rowIndex: trueIndex, colIndex: colIndex, deduced: deducedCopy })
-    }
-  }
-
-  return deducedCopy
+  return grid
 }
 
 generateClue()
--- operators.js
+++ operators.js
@@ -1,12 +1,173 @@
-[
-  [
-    [[null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null]], 
-    [[null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null]]], 
-    [[[null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null]]
-  ], []]
+groups = [
+  ["a", "b", "c", "d"],
+  [1, 2, 3, 4],
+  [10, 20, 30, 40],
+  [110, 220, 330, 440]
+]
 
-  [
-    [[null,null,null,null],[null,null,null,null],[null,null,null,null],[null,null,null,null]],
-    [[null,null,null,null],[null,null,null,null],[null,null,null,null],[null,null,null,null]],
-    [[null,null,null,null],[null,null,null,null],[null,null,null,null],[null,null,null,null]]
-  ]
+// const out = keys.map((a, ai) => (
+//   keys.slice(1, keys.length).map((b, bi) => `-${a}=${b}-`)
+// ))
+
+// console.log(out)
+
+
+groups = [
+  ["a", "b"],
+  [1, 2],
+  [10, 20],
+  [100, 200]
+]
+
+groups = [
+  [100, 200], 
+  [10, 20], 
+  [1, 2], 
+  ["a", "b"]
+]
+
+const out = groups.map((a,ai) => (
+  // 100 200
+  makeGridOfSize(2, null)
+))
+console.log(out)
+
+let ar = []
+let prevVal = null
+while (groups.length) {
+  const current = groups.pop()
+  prevVal = makeGridOfSize(2, prevVal)
+  ar.push(prevVal)
+  
+}
+console.log(JSON.stringify(prevVal))
+
+function makeGridOfSize(size, value=null) {
+  return Array.from({ length: size }, () => (Array.from({ length: size }, () => value)
+  ))
+}
+
+groups = [
+  [100, 200], 
+  [10, 20], 
+  [1, 2], 
+  ["a", "b"]
+]
+
+for (let index = 0; index < groups.length; index++) {
+}
+
+// [
+//   [
+//     [
+//       ["a", 1, 10, 100], ["b", 1, 10, 100],
+//       ["a", 2, 10, 100], ["b", 2, 10, 100]
+//     ],
+//     [
+//       ["a", 1, 20, 100], ["b", 1, 20, 100],
+//       ["a", 2, 20, 100], ["b", 2, 20, 100]
+//     ]
+//   ],
+//   [
+//     [
+//       ["a", 1, 10, 200], ["b", 1, 10, 200],
+//       ["a", 2, 10, 200], ["b", 2, 10, 200]
+//     ],
+//     [
+//       ["a", 1, 20, 200], ["b", 1, 20, 200],
+//       ["a", 2, 20, 200], ["b", 2, 20, 200]
+//     ]
+//   ]
+// ]
+
+
+
+
+
+// [
+//   ["a", "b"],
+//   [1, 2],
+//   [10, 20],
+//   [110, 220]
+// ]
+
+// [
+//   ["a", 1], ["b", 1],
+//   ["a", 2], ["b", 2]
+// ]
+
+// [
+//   [
+//     ["a", 1, 10], ["a", 2, 10],
+//     ["b", 1, 10], ["b", 2, 10]
+//   ],
+//   [
+//     ["a", 1, 20], ["a", 2, 20],
+//     ["b", 1, 20], ["b", 2, 20]
+//   ]
+// ]
+
+// [
+//   [
+//     [
+//       ["a", 1, 10, 100], ["a", 2, 10, 100],
+//       ["b", 1, 10, 100], ["b", 2, 10, 100]
+//     ],
+//     [
+//       ["a", 1, 20, 100], ["a", 2, 20, 100],
+//       ["b", 1, 20, 100], ["b", 2, 20, 100]
+//     ]
+//   ],
+//   [
+//     [
+//       ["a", 1, 10, 200], ["a", 2, 10, 200],
+//       ["b", 1, 10, 200], ["b", 2, 10, 200]
+//     ],
+//     [
+//       ["a", 1, 20, 200], ["a", 2, 20, 200],
+//       ["b", 1, 20, 200], ["b", 2, 20, 200]
+//     ]
+//   ]
+// ]
+
+// ---
+
+// [
+//   ["a", 1], ["b", 1],
+//   ["a", 2], ["b", 2]
+// ]
+
+// [
+//   [
+//     ["a", 1, 10], ["b", 1, 10],
+//     ["a", 2, 10], ["b", 2, 10]
+//   ],
+//   [
+//     ["a", 1, 20], ["b", 1, 20],
+//     ["a", 2, 20], ["b", 2, 20]
+//   ]
+// ]
+
+// [
+//   [
+//     [
+//       ["a", 1, 10, 100], ["b", 1, 10, 100],
+//       ["a", 2, 10, 100], ["b", 2, 10, 100]
+//     ],
+//     [
+//       ["a", 1, 20, 100], ["b", 1, 20, 100],
+//       ["a", 2, 20, 100], ["b", 2, 20, 100]
+//     ]
+//   ],
+//   [
+//     [
+//       ["a", 1, 10, 200], ["b", 1, 10, 200],
+//       ["a", 2, 10, 200], ["b", 2, 10, 200]
+//     ],
+//     [
+//       ["a", 1, 20, 200], ["b", 1, 20, 200],
+//       ["a", 2, 20, 200], ["b", 2, 20, 200]
+//     ]
+//   ]
+// ]
+
