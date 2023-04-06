03bd06a686a24593edf8b7d3242a082ae796166b
skedwards88 <skedwards88@github.com>
2023-04-06 08:46:50 -0700

    puzzle working for not clues...if you wait long enough

--- code4.js
+++ code4.js
@@ -1,36 +1,3 @@
-const categories = [
-  ["Colin", "Sarah", "Meme", "Fefe"],
-  [1, 2, 3, 4, 5],
-  ["fly", "back", "breast", "free"],
-  ["red", "blue", "green", "yellow"],
-];
-
-// todo randomize later
-let solution = categories.map((i, index) => [
-  categories[0][index],
-  categories[1][index],
-  categories[2][index],
-  categories[3][index],
-]);
-
-const numItems = categories[0].length;
-const numCats = 4;
-let solutionMatrix = {};
-for (let catIndex = 0; catIndex < numCats; catIndex++) {
-  for (let vsIndex = catIndex + 1; vsIndex < numCats; vsIndex++) {
-    const grid = Array.from({ length: numItems }, () =>
-      Array.from({ length: numItems }, () => null)
-    );
-    const rowLabels = categories[catIndex];
-    const colLabels = categories[vsIndex];
-    solutionMatrix[`${catIndex}v${vsIndex}`] = {
-      rowLabels: rowLabels,
-      colLabels: colLabels,
-      grid: grid,
-    };
-  }
-}
-
 // return a random element from a given array
 function pickRandom(inputArray) {
   return inputArray[Math.floor(Math.random() * inputArray.length)];
@@ -44,6 +11,7 @@ function pickRandomIndex(inputArray) {
 // "not" clue
 // e.g. "colin is not blue"
 function getNotClue(solution) {
+  console.log("in getNotClue");
   let solutionIndexes = Array.from(solution.keys());
   let clueIndexes = Array.from(solution[0].keys());
 
@@ -54,7 +22,7 @@ function getNotClue(solution) {
   const catIndex1 = pickRandom(clueIndexes);
   const item1 = solution1[catIndex1];
 
-  // delete the used indexes
+  // delete the used indexes so that we don't re-pick them
   solutionIndexes.splice(solutionIndex1, 1);
   clueIndexes.splice(catIndex1, 1);
 
@@ -85,9 +53,7 @@ function getNotClue(solution) {
     const rowIndex = solutionRows.indexOf(rowItem);
     const colIndex = solutionCols.indexOf(colItem);
 
-    console.log(JSON.stringify(newSolutionGrid));
     newSolutionGrid[rowIndex][colIndex] = false;
-    console.log(JSON.stringify(newSolutionGrid));
 
     newSolutionMatrix[solutionKey].grid = newSolutionGrid;
     console.log(
@@ -102,4 +68,106 @@ function getNotClue(solution) {
   };
 }
 
-// getNotClue(solution);
+function puzzleSolvedQ(solutionMatrix) {
+  for (const key in solutionMatrix) {
+    const grid = solutionMatrix[key].grid;
+    for (const row of grid) {
+      for (const item of row) {
+        if (item === null) {
+          hasNull = true;
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+function getUsefulClue(solution, solutionMatrix) {
+  console.log("IN getUsefulClue");
+  let foundUsefulClue = false;
+  let clue;
+  let newSolutionMatrix;
+
+  while (!foundUsefulClue) {
+    // get a clue
+    clue = getNotClue(solution); // todo randomize clue type
+
+    // see if it changes the solutionMatrix
+    newSolutionMatrix = clue.clueLogic(solutionMatrix);
+    // todo write cleaner comparison method
+    // could also check this at the time the clue was applied--more efficient but maybe less readable
+    foundUsefulClue =
+      JSON.stringify(newSolutionMatrix) !== JSON.stringify(solutionMatrix);
+  }
+
+  return {
+    clue: clue,
+    newSolutionMatrix: newSolutionMatrix,
+  };
+}
+
+const categories = [
+  ["Colin", "Sarah", "Meme", "Fefe"],
+  [1, 2, 3, 4, 5],
+  ["fly", "back", "breast", "free"],
+  ["red", "blue", "green", "yellow"],
+];
+
+function generateSolution(categories) {
+  // todo randomize later
+  let solution = categories.map((i, index) => [
+    categories[0][index],
+    categories[1][index],
+    categories[2][index],
+    categories[3][index],
+  ]);
+
+  return solution;
+}
+
+function generatePuzzle(categories) {
+  const solution = generateSolution(categories);
+
+  // initialize the solution matrix
+  const numItems = categories[0].length;
+  const numCats = categories.length;
+  let solutionMatrix = {};
+  for (let catIndex = 0; catIndex < numCats; catIndex++) {
+    for (let vsIndex = catIndex + 1; vsIndex < numCats; vsIndex++) {
+      const grid = Array.from({ length: numItems }, () =>
+        Array.from({ length: numItems }, () => null)
+      );
+      const rowLabels = categories[catIndex];
+      const colLabels = categories[vsIndex];
+      solutionMatrix[`${catIndex}v${vsIndex}`] = {
+        rowLabels: rowLabels,
+        colLabels: colLabels,
+        grid: grid,
+      };
+    }
+  }
+
+  let clues = [];
+  let puzzleIsSolved = false;
+  let clue;
+  let newSolutionMatrix = solutionMatrix;
+  let count = 0;
+
+  while (!puzzleIsSolved && count < 100) {
+    count++;
+    console.log(`loop ${count}`);
+    //todo this works, but as the puzzle gets more solved, it is less likely that a random clue is helpful
+    // maybe adding more clue types will help
+    // but may also need to make the code smarter to have a more targeted search
+    (
+      ({ clue, newSolutionMatrix } = getUsefulClue(solution, newSolutionMatrix))
+    );
+    clues = [...clues, clue];
+    puzzleIsSolved = puzzleSolvedQ(newSolutionMatrix);
+  }
+
+  console.log(JSON.stringify(newSolutionMatrix));
+}
+
+generatePuzzle(categories);
