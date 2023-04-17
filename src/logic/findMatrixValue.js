826ea63e9be487762aa91aaa5162faadba358037
skedwards88 <skedwards88@github.com>
2023-04-16 19:50:31 -0700

    add function to look up value from matrix

---
+++ src/logic/findMatrixValue.js
@@ -0,0 +1,26 @@
+
+export function findMatrixValue(matrix, itemA, itemB) {
+  // todo add tests
+  for (const key in matrix) {
+    if (
+      matrix[key].rowLabels.includes(itemA) &&
+      matrix[key].colLabels.includes(itemB)
+    ) {
+      const rowIndex = matrix[key].rowLabels.indexOf(itemA)
+      const colIndex = matrix[key].colLabels.indexOf(itemB)
+      return matrix[key].grid[rowIndex][colIndex];
+    }
+    if (
+      matrix[key].rowLabels.includes(itemB) &&
+      matrix[key].colLabels.includes(itemA)
+    ) {
+      
+      const rowIndex = matrix[key].rowLabels.indexOf(itemB)
+      const colIndex = matrix[key].colLabels.indexOf(itemA)
+      return matrix[key].grid[rowIndex][colIndex];
+    }
+  }
+  throw new Error(
+    `Did not find matrix entry corresponding to ${itemA} vs ${itemB}`
+  );
+}
---
+++ src/logic/findMatrixValue.test.js
@@ -0,0 +1,82 @@
+import {findMatrixValue}  from "./findMatrixValue.js"
+
+const matrix = {
+  "0v1": {
+    rowLabels: ["Colin", "Sarah", "Fefe", "Meme"],
+    colLabels: [1, 2, 3, 4],
+    grid: [
+      [null, null, null, null],
+      [null, null, null, null],
+      [null, null, null, null],
+      [null, null, null, null],
+    ],
+  },
+  "0v2": {
+    rowLabels: ["Colin", "Sarah", "Fefe", "Meme"],
+    colLabels: ["fly", "back", "breast", "free"],
+    grid: [
+      [null, null, null, null],
+      [null, null, null, null],
+      [null, null, null, null],
+      [null, null, null, null],
+    ],
+  },
+  "0v3": {
+    rowLabels: ["Colin", "Sarah", "Fefe", "Meme"],
+    colLabels: ["red", "blue", "green", "yellow"],
+    grid: [
+      [null, null, null, null],
+      [null, null, false, null],
+      [null, null, null, null],
+      [null, null, null, null],
+    ],
+  },
+  "1v2": {
+    rowLabels: [1, 2, 3, 4],
+    colLabels: ["fly", "back", "breast", "free"],
+    grid: [
+      [true, null, null, null],
+      [null, null, null, null],
+      [null, null, null, null],
+      [null, null, null, null],
+    ],
+  },
+  "1v3": {
+    rowLabels: [1, 2, 3, 4],
+    colLabels: ["red", "blue", "green", "yellow"],
+    grid: [
+      [null, null, null, null],
+      [null, null, null, null],
+      [null, null, null, null],
+      [null, null, null, null],
+    ],
+  },
+  "2v3": {
+    rowLabels: ["fly", "back", "breast", "free"],
+    colLabels: ["red", "blue", "green", "yellow"],
+    grid: [
+      [null, null, null, null],
+      [null, null, null, null],
+      [null, null, null, null],
+      [null, null, null, null],
+    ],
+  },
+};
+
+describe("findMatrixValue", () => {
+  test("finds the value that corresponds to the intersection of two items", () => {
+    expect(findMatrixValue(matrix, "Sarah", "green")).toBe(false);
+    expect(findMatrixValue(matrix, "green", "Sarah")).toBe(false);
+    expect(findMatrixValue(matrix, 1, "fly")).toBe(true);
+    expect(findMatrixValue(matrix, "fly", 1)).toBe(true);
+    expect(findMatrixValue(matrix, "fly", "yellow")).toBe(null);
+    expect(findMatrixValue(matrix, "yellow", "fly")).toBe(null);
+  });
+
+
+  test("throws an error if the intersection is not found", () => {
+    expect(() => findMatrixValue(matrix, "dog", "Sarah")).toThrow(
+      "Did not find matrix entry corresponding to dog vs Sarah"
+    );
+  });
+});
