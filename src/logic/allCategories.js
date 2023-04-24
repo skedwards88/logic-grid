3594654b98e7f89af586798259099b3220c5b738
skedwards88 <skedwards88@github.com>
2023-04-24 08:07:40 -0700

    modify categories

--- src/logic/allCategories.js
+++ src/logic/allCategories.js
@@ -2,7 +2,7 @@ export const allCategories = {
   // todo later add type, display name, checks to make sure that don't have cats that are too similar?
   firstName: ["Colin", "Sarah", "Fefe", "Meme", "Bob", "Joe"],
   order: [1, 2, 3, 4, 5], //todo some orders can't be randomized
-  stroke: ["fly", "back", "breast", "free", "im"],
-  lastName: ["Smith", "Yu", "Thom", "Edwards", "Wong"],
   color: ["red", "blue", "green", "yellow", "orange"],
+  stroke: ["fly", "back", "breast", "free", "IM"],
+  lastName: ["Ash", "Doe", "Thom", "Mills", "Wong"],
 };
