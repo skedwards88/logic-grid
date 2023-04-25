9d46d2d7b02845a1d5ae0edea6192521d4f1067c
skedwards88 <skedwards88@github.com>
2023-04-25 08:23:31 -0700

    modify class name

--- src/components/LabelSet.js
+++ src/components/LabelSet.js
@@ -2,5 +2,5 @@ import React from "react";
 
 export default function LabelSet({labels, id, type}) {
   const labelDivs = labels.map((label) => <div>{label}</div>);
-  return <div className={`${type}Label Label${id}`}>{labelDivs}</div>;
+  return <div className={`${type}Label label${id} size${labels.length}`}>{labelDivs}</div>;
 }
