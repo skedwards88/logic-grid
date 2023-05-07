1c213ec52221fcdbd90670190711070360488214
skedwards88 <skedwards88@github.com>
2023-05-07 10:08:32 -0700

    choose random categories and values

--- src/logic/chooseCategories.js
+++ src/logic/chooseCategories.js
@@ -1,18 +1,26 @@
 import {allCategories} from "./allCategories.js";
 import {pickRandom} from "./helpers/pickRandom.js";
+import {shuffleArray} from "./helpers/shuffleArray.js";
 
 export function chooseCategories(numCats, numItemsPerCat) {
   // returns a list of category labels and description templates
 
   const categorySet = pickRandom(allCategories);
 
-  // todo ensure that all categories have at least numItemsPerCat
-  const categoryNames = Object.keys(categorySet).slice(0, numCats); // todo shuffle first to get random + add test for randomness
+  const categoryNames = shuffleArray(Object.keys(categorySet)).slice(
+    0,
+    numCats,
+  );
 
   let categoryLabelsAndTemplates = [];
   for (const categoryName of categoryNames) {
-    const possibleValues = categorySet[categoryName].values;
-    const selectedValues = possibleValues.slice(0, numItemsPerCat); //todo shuffle first to get random  + add test for randomness
+    const possibleValues = shuffleArray(categorySet[categoryName].values);
+    const selectedValues = possibleValues.slice(0, numItemsPerCat);
+    // sort the values to make more user friendly
+    // note: the numeric clues also assume that the numeric labels are sorted
+    typeof (selectedValues[0] === "number")
+      ? selectedValues.sort((a, b) => a - b)
+      : selectedValues.sort();
     const categoryInfo = {
       labels: selectedValues,
       descriptionTemplates: categorySet[categoryName].descriptionTemplates,
