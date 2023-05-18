15937fd4872e1ad2090af40c5be5cc71d4ee8b46
skedwards88 <skedwards88@github.com>
2023-05-18 16:20:46 -0700

    robustify tests

--- src/logic/helpers/pickRandom.test.js
+++ src/logic/helpers/pickRandom.test.js
@@ -1,17 +1,48 @@
 import {pickRandom, pickRandomIndex, pickRandoms} from "./pickRandom";
 
 describe("pickRandom", () => {
-  test("returns a random element from the input array", () => {
+  test("returns an element that was in the input array", () => {
     const inputArray = ["act", "bat", "cat", "dog", "fish"];
     const randomElement = pickRandom(inputArray);
     expect(inputArray).toContain(randomElement);
   });
 
   test("returns a random element from the input array", () => {
-    const inputArray = ["act", "bat", "cat", "dog", "fish"];
-    const numberOfIterations = 1000;
+    const inputArray = [
+      "act",
+      "bat",
+      "cat",
+      "dog",
+      "fish",
+      "monkey",
+      "red",
+      "orange",
+      "yellow",
+      "green",
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+    ];
+    const numberOfIterations = 10000;
     const expectedDistribution = 1 / inputArray.length;
-    const counts = inputArray.reduce((dict, key) => ({...dict, [key]: 0}), {});
+    let counts = inputArray.reduce((dict, key) => ({...dict, [key]: 0}), {});
 
     for (let i = 0; i < numberOfIterations; i++) {
       const item = pickRandom(inputArray);
@@ -26,7 +57,7 @@ describe("pickRandom", () => {
 });
 
 describe("pickRandoms", () => {
-  test("returns n random elements from the input array", () => {
+  test("returns n elements from the input array", () => {
     const inputArray = ["act", "bat", "cat", "dog", "fish"];
     const randomElements = pickRandoms(inputArray, 3);
 
@@ -43,8 +74,39 @@ describe("pickRandoms", () => {
     expect(randomElements.length).toBe(inputArray.length);
   });
 
-  test("pickRandoms returns random values", () => {
-    const inputArray = ["act", "bat", "cat", "dog", "fish"];
+  test("returns random values", () => {
+    const inputArray = [
+      "act",
+      "bat",
+      "cat",
+      "dog",
+      "fish",
+      "monkey",
+      "red",
+      "orange",
+      "yellow",
+      "green",
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+    ];
     const numberOfItems = 3;
     const numberOfIterations = 1000;
     const expectedDistribution = 1 / inputArray.length;
@@ -66,29 +128,60 @@ describe("pickRandoms", () => {
 });
 
 describe("pickRandomIndex", () => {
-  test("returns a random index from the input array", () => {
-    const inputArray = [1, 2, 3, 4, 5];
+  test("returns an index from the input array", () => {
+    const inputArray = ["act", "bat", "cat", "dog", "fish"];
     const randomIndex = pickRandomIndex(inputArray);
     expect(randomIndex).toBeGreaterThanOrEqual(0);
     expect(randomIndex).toBeLessThan(inputArray.length);
   });
 
   test("returns a random index from the input array", () => {
-    const inputArray = [1, 2, 3, 4, 5];
-    const numIterations = 100000;
-    const expectedDistribution = inputArray.map(
-      () => numIterations / inputArray.length,
+    const inputArray = [
+      "act",
+      "bat",
+      "cat",
+      "dog",
+      "fish",
+      "monkey",
+      "red",
+      "orange",
+      "yellow",
+      "green",
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+    ];
+    const numberOfIterations = 10000;
+    const expectedDistribution = 1 / inputArray.length;
+    let counts = inputArray.reduce(
+      (dict, key, index) => ({...dict, [index]: 0}),
+      {},
     );
-    const observedDistribution = inputArray.map(() => 0);
-    for (let i = 0; i < numIterations; i++) {
+
+    for (let i = 0; i < numberOfIterations; i++) {
       const randomIndex = pickRandomIndex(inputArray);
-      observedDistribution[randomIndex]++;
+      counts[randomIndex] = counts[randomIndex] + 1;
     }
-    const chiSquared = observedDistribution.reduce((sum, observed, index) => {
-      const expected = expectedDistribution[index];
-      return sum + (observed - expected) ** 2 / expected;
-    }, 0);
-    const criticalValue = 11.07; // for 4 degrees of freedom and alpha = 0.05
-    expect(chiSquared).toBeLessThan(criticalValue);
+    for (const key in counts) {
+      const actualDistribution = counts[key] / numberOfIterations;
+      expect(actualDistribution).toBeCloseTo(expectedDistribution, 1);
+    }
   });
 });
