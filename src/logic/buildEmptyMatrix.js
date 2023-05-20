c6e0a1bc8e11291f8363880862233f7471e0c626
skedwards88 <skedwards88@github.com>
2023-05-20 14:34:04 -0700

    just build empty matrix based on solution

--- src/logic/buildEmptyMatrix.js
+++ src/logic/buildEmptyMatrix.js
@@ -1,28 +1,12 @@
-export function buildEmptyMatrix(categoryLabelsAndTemplates) {
-  const numItems = categoryLabelsAndTemplates[0].labels.length;
-  const numCats = categoryLabelsAndTemplates.length;
-  let derivedMatrix = {};
+export function buildEmptyMatrix(solutionMatrix) {
+  const matrixCopy = JSON.parse(JSON.stringify(solutionMatrix));
 
-  for (let categoryIndexA = 0; categoryIndexA < numCats; categoryIndexA++) {
-    for (
-      let categoryIndexB = categoryIndexA + 1;
-      categoryIndexB < numCats;
-      categoryIndexB++
-    ) {
-      const grid = Array.from({length: numItems}, () =>
-        Array.from({length: numItems}, () => null),
-      );
-      const rowLabels = categoryLabelsAndTemplates[categoryIndexA].labels;
-        categoryLabelsAndTemplates[categoryIndexA].descriptionTemplates;
-      const colLabels = categoryLabelsAndTemplates[categoryIndexB].labels;
-        categoryLabelsAndTemplates[categoryIndexB].descriptionTemplates;
-
-      derivedMatrix[`category${categoryIndexA}_category${categoryIndexB}`] = {
-        rowLabels: rowLabels,
-        colLabels: colLabels,
-        grid: grid,
-      };
-    }
+  for (const key in matrixCopy) {
+    matrixCopy[key].grid = matrixCopy[key].grid.map((row) =>
+      row.map((item) => null),
+    );
+    delete matrixCopy[key].rowDescriptionTemplates;
+    delete matrixCopy[key].colDescriptionTemplates;
   }
-  return derivedMatrix;
+  return matrixCopy;
 }
