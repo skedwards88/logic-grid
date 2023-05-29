b516db2df558e37c4fc119becc5cd5f1acbb08c5
skedwards88 <skedwards88@github.com>
2023-05-28 17:36:49 -0700

    clean up cell touch handling

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -8,77 +8,74 @@ export default function Cell({
   easyTrue,
   dispatchGameState,
 }) {
-  const [timer, setTimer] = React.useState(null);
-
   let className = "cell";
   if (value === true) {
     className += " true";
   } else if (value === false) {
     className += " false";
   }
+  console.log("RENDER");
+
+  const timerID = React.useRef();
+  const pressIsLong = React.useRef();
+
+  function handleTouchStart() {
+    console.log("touch start");
+    // When the press initiates, it isn't long (yet)
+    pressIsLong.current = false;
+    timerID.current = setTimeout(() => {
+      console.log("long press");
+      // If the press exceeds the timeout, it is long
+      pressIsLong.current = true;
+      if (easyTrue) {
+        dispatchGameState({
+          action: "utilizeEasyTrue",
+          gridID: gridID,
+          rowIndex: rowIndex,
+          columnIndex: columnIndex,
+          tempFrom: "long touch",
+        });
+      }
+    }, 500);
+  }
+
+  function handleTouchEnd() {
+    console.log("touch end");
+    if (timerID.current) {
+      clearTimeout(timerID.current);
+    }
+  }
+
+  function handleClick() {
+    console.log("click");
+    if (pressIsLong.current) {
+      // If the press/click was treated as a long press, don't do anything
+      console.log("Not clicking--already long press");
+      return;
+    }
+    clearTimeout(timerID.current);
+
+    dispatchGameState({
+      action: "changeCellState",
+      gridID: gridID,
+      rowIndex: rowIndex,
+      columnIndex: columnIndex,
+      tempFrom: "click",
+    });
+  }
+
   return (
     <div
       className={className}
       // ios13 doesn't respect context menu event, so need to monitor long touch instead
-      // but desktop doesn't use touch, so also need context menu
-      onTouchStart={(event, passive=false) => {
-        // event.preventDefault();
-        // console.log('touch start')
-        setTimer(
-          setTimeout(() => {
-            console.log('timeout')
-            if (easyTrue) {
-              
-              dispatchGameState({
-                action: "utilizeEasyTrue",
-                gridID: gridID,
-                rowIndex: rowIndex,
-                columnIndex: columnIndex,
-                tempFrom: "long touch"
-              });
-            }
-          }, 1000),
-        );
-      }}
-      onTouchEnd={(event) => {
-        // event.preventDefault();
-        console.log('touch end')
-        if (timer) {
-          clearTimeout(timer);
-        }
-      }}
-      onTouchMove={(event) => {
-        // event.preventDefault();
-        console.log('touch move')
-        if (timer) {
-          clearTimeout(timer);
-        }
-      }}
+      onTouchStart={handleTouchStart}
+      onMouseDown={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
+      onMouseUp={handleTouchEnd}
+      onTouchMove={handleTouchEnd}
+      onClick={handleClick}
       onContextMenu={(event) => {
         event.preventDefault();
-        if (!timer) {
-          console.log('menu')
-          if (easyTrue) {
-            dispatchGameState({
-              action: "utilizeEasyTrue",
-              gridID: gridID,
-              rowIndex: rowIndex,
-              columnIndex: columnIndex,
-              tempFrom: "menu"
-            });
-          }
-        }
-      }}
-      onClick={(event) => {
-        event.preventDefault();
-        console.log('click')
-        dispatchGameState({
-          action: "changeCellState",
-          gridID: gridID,
-          rowIndex: rowIndex,
-          columnIndex: columnIndex,
-          tempFrom: "click",
-        });
       }}
     ></div>
   );
