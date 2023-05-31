6d45f497607fe5eded7aec716215174e55389d34
skedwards88 <skedwards88@github.com>
2023-05-31 07:46:17 -0700

    randomize solution

--- src/logic/generateSolution.js
+++ src/logic/generateSolution.js
@@ -1,14 +1,23 @@
+import {shuffleArray} from "./helpers/shuffleArray";
+
 export function generateSolution(categoryLabels) {
   // given the lists of labels (e.g. [[a,b,c,d],[1,2,3,4],[w, x,y,z]])
   // combine them into a "solution" (e.g. [[a,1,w],[b,2,x],[c,4,z],[d,3,y]])
   const numCats = categoryLabels.length;
   const numItemsPerCat = categoryLabels[0].length;
 
+  const shuffledCategoryLabels = categoryLabels.map((labels) =>
+    shuffleArray(labels),
+  );
+
   let solutions = [];
   for (let solutionIndex = 0; solutionIndex < numItemsPerCat; solutionIndex++) {
     let solution = [];
     for (let categoryIndex = 0; categoryIndex < numCats; categoryIndex++) {
-      solution = [...solution, categoryLabels[categoryIndex][solutionIndex]]; // todo randomize solution
+      solution = [
+        ...solution,
+        shuffledCategoryLabels[categoryIndex][solutionIndex],
+      ];
     }
     solutions = [...solutions, solution];
   }
