509403c443e753536402a0795ab28f8ff8a0a6ff
skedwards88 <skedwards88@github.com>
2024-02-19 18:08:01 -0800

    standardize config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const WorkboxPlugin = require("workbox-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const packageJson = require("./package.json");
 
 module.exports = (env, argv) => {
   if (argv.mode === "development") {
@@ -70,10 +71,11 @@ module.exports = (env, argv) => {
   });
 
   const serviceWorkerPlugin = new WorkboxPlugin.GenerateSW({
-    // these options encourage the ServiceWorkers to get in there fast
-    // and not allow any straggling "old" SWs to hang around
+    // This helps ensure that all pages will be controlled by a service worker immediately after that service worker activates
     clientsClaim: true,
+    // This skips the service worker waiting phase, meaning the service worker activates as soon as it's finished installing
     skipWaiting: true,
+    cacheId: `logic-grid-${packageJson.version}`,
   });
 
   const plugins =
@@ -97,7 +99,7 @@ module.exports = (env, argv) => {
           use: ["style-loader", "css-loader"],
         },
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
           type: "asset/resource",
         },
       ],
@@ -110,12 +112,13 @@ module.exports = (env, argv) => {
       clean: true, // removes unused files from output dir
     },
     performance: {
+      // special case to cache scenarios for offline play
       maxEntrypointSize: 400000, // bytes
+      // special case to cache scenarios for offline play
       maxAssetSize: 400000, // bytes
     },
     devServer: {
       static: "./dist",
-      historyApiFallback: true,
     },
     plugins: plugins,
   };
