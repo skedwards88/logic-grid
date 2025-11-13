fd83024eb51fd45592dad743a4cb6422123cedfd
Sarah Edwards <skedwards88@gmail.com>
2025-11-13 02:28:15 -0700

    fix service worker for new custom domain

--- src/index.js
+++ src/index.js
@@ -7,11 +7,8 @@ import "@skedwards88/shared-components/src/styles/Install.css";
 import {MetadataContextProvider} from "@skedwards88/shared-components/src/components/MetadataContextProvider";
 
 if (process.env.NODE_ENV !== "development" && "serviceWorker" in navigator) {
-  const path =
-    location.hostname === "localhost"
-      ? "/service-worker.js"
-      : "/logic-grid/service-worker.js";
-  const scope = location.hostname === "localhost" ? "" : "/logic-grid/";
+  const path = "/service-worker.js";
+  const scope = "";
   window.addEventListener("load", () => {
     navigator.serviceWorker
       .register(path, {scope: scope})
